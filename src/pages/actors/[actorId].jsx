1b0e19ec70301e7e4234c56384d4691cf4d2a517
Mouloud Mecheter <mouloudmecheter@gmail.com>
2023-09-16 23:14:48 +0100

    style(actors page and single actor page): style the actors page and single actor page and clean code

--- src/pages/actors/[actorId].jsx
+++ src/pages/actors/[actorId].jsx
@@ -2,54 +2,40 @@ import React from "react"
 import daisyui from "daisyui"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
-import { data } from "autoprefixer"
 import Link from "next/link"
+import { fetcher } from "@/utils/API"
 
 function ActorDetailsPage() {
+  // Retrieve the actor's ID from the route
   const router = useRouter()
-  const { id } = router.query // Retrieve the actor's ID from the route
-  const [actorData, setActorData] = useState(null)
-
-  // Fetch actor data using the ID and display it
-  const fetchActorData = async (actorId) => {
-    const options = {
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMmZlZjIwMmQ0MjFhNTYxNzg2YzU3ODQ5YzRhZmJjMyIsInN1YiI6IjY1MDFiNjcxNmEyMjI3MDBjM2I2YWIxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XJjyQmVwx0bDppP1jD0WnR_WV0eH7kBhZBRVRQFEMhQ",
-      },
-    }
-    const res = await fetch(
-      `https://api.themoviedb.org/3/person/${actorId}`,
-      options,
-    )
-    const posts = await res.json()
-    setActorData(posts)
-  }
+  const { id } = router.query
 
   useEffect(() => {
     if (!id) return
-    fetchActorData(id)
-    console.log("Actor ID:", id)
+    getActorData(id)
+    getRelatedMovies(id)
   }, [id])
 
-  // Fetch Actor related movies using the id
+  // useState for actorData
+  const [actorData, setActorData] = useState(null)
+
+  // Fetch actor data using the ID and display it
+  async function getActorData(actorId) {
+    const fetchActorData = await fetcher(`person/${actorId}`)
+    setActorData(fetchActorData)
+  }
+
+  // useState for relatedMovies
   const [relatedMovies, setRelatedMovies] = useState([])
 
-  useEffect(() => {
-    // Fetch related movies and set them in the state
-    fetcher(id)
-      .then((data) => {
-        setRelatedMovies(data.cast)
-      })
-      .catch((error) => {
-        // Handle any errors
-        console.error(error)
-      })
-  }, [id])
+  // Fetch Actor related movies using the id
+  async function getRelatedMovies(actorId) {
+    const actorRelatedMovies = await fetcher(`person/${actorId}/movie_credits`)
+    setRelatedMovies(actorRelatedMovies.cast)
+  }
 
   return (
-    <main style={{ backgroundColor: "#35393e" }}>
+    <main style={{ backgroundImage: "url(/imageBackground.jpg)" }}>
       <div className="container mx-auto p-4">
         <div className="lg:flex">
           {/* Actor Image (25% of screen width) */}
@@ -61,19 +47,25 @@ function ActorDetailsPage() {
                 }
                 alt="Actor Image"
                 className="w-full h-full object-cover"
-                style={{ border: "3px solid white" }}
+                style={{ border: "2px solid white" }}
               />
             </div>
           </div>
 
           {/* Actor Information (75% of screen width) */}
           <div className="lg:w-3/4 p-4">
-            <div className="card bg-base-100 shadow-lg">
-              <div className="card-body flex flex-col justify-between h-full">
+            <div
+              className="card bg-base-100 shadow-lg"
+              style={{ backgroundColor: "transparent" }}
+            >
+              <div
+                className="card-body flex flex-col justify-between h-full"
+                style={{ color: "white" }}
+              >
                 <div>
                   <h2
                     className="card-title"
-                    style={{ fontSize: 30, marginBottom: 10 }}
+                    style={{ fontSize: 50, marginBottom: 20 }}
                   >
                     {actorData?.name}
                   </h2>
@@ -96,7 +88,7 @@ function ActorDetailsPage() {
       {/* Related Movies*/}
       <div
         className="mt-8"
-        style={{ marginLeft: 91, marginRight: 91, marginBottom: 20 }}
+        style={{ marginLeft: 91, marginRight: 91, paddingBottom: 20 }}
       >
         <h2 className="text-2xl font-semibold mb-4" style={{ color: "white" }}>
           Related Movies
@@ -106,7 +98,7 @@ function ActorDetailsPage() {
             <Link key={movie.name} href={`/movies/movieId?id=${movie.id}`}>
               <div
                 key={movie.id}
-                className="card bg-base-100 shadow-lg div-hover:hover"
+                className="card bg-base-100 shadow-lg"
                 style={{
                   backgroundImage: "url(/imageBackground.jpg)",
                   border: "2px solid white",
@@ -135,21 +127,6 @@ function ActorDetailsPage() {
       </div>
     </main>
   )
-}
-
-export async function fetcher(apiRoute) {
-  const url = `https://api.themoviedb.org/3/person/${apiRoute}/movie_credits`
-
-  const options = {
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMmZlZjIwMmQ0MjFhNTYxNzg2YzU3ODQ5YzRhZmJjMyIsInN1YiI6IjY1MDFiNjcxNmEyMjI3MDBjM2I2YWIxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XJjyQmVwx0bDppP1jD0WnR_WV0eH7kBhZBRVRQFEMhQ",
-    },
-  }
-  const response = await fetch(url, options)
-  const data = await response.json()
-  return data
 }
 
 export default ActorDetailsPage
--- src/pages/actors/index.jsx
+++ src/pages/actors/index.jsx
@@ -1,33 +1,49 @@
 import React from "react"
 import daisyui from "daisyui"
 import Link from "next/link"
-import { data } from "autoprefixer"
+import { fetcher } from "@/utils/API"
+
+export async function getStaticProps() {
+  const popularActors = await fetcher("person/popular?language=en-US&page=1") // Call the fetcher function with the API route
+
+  return {
+    props: {
+      popularActors,
+    },
+  }
+}
 
 function Actors({ popularActors }) {
-  console.log(popularActors.results)
-
   return (
-    <main style={{ backgroundColor: "#1B1B1D " }}>
+    <main style={{ backgroundColor: "#35393e" }}>
       <div
         className="container mx-auto mt-5"
-        style={{ backgroundColor: "#1B1B1D " }}
+        style={{ marginTop: 0, paddingTop: 20 }}
       >
         <h1
           className="text-3xl font-semibold mb-5"
-          style={{ color: "white", textAlign: "center" }}
+          style={{ color: "white", textAlign: "center", fontSize: 40 }}
         >
           Popular Actors
         </h1>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
           {popularActors.results.map((actor) => (
             <Link key={actor.name} href={`/actors/actorId?id=${actor.id}`}>
-              <div key={actor.name} className="bg-white p-4 rounded shadow">
+              <div
+                key={actor.name}
+                className="bg-white p-4 rounded shadow"
+                style={{ backgroundImage: "url(/imageBackground.jpg)" }}
+              >
                 <img
                   src={"https://image.tmdb.org/t/p/w500" + actor.profile_path}
                   alt={actor.name}
                   className="w-full h-auto rounded-md mb-2"
+                  style={{ border: "2px solid white" }}
                 />
-                <p className="text-lg font-semibold">{actor.name}</p>
+                <p className="text-lg font-semibold" style={{ color: "white" }}>
+                  {actor.name}
+                </p>
               </div>
             </Link>
           ))}
@@ -35,27 +51,6 @@ function Actors({ popularActors }) {
       </div>
     </main>
   )
-}
-
-export async function getStaticProps() {
-  const url =
-    "https://api.themoviedb.org/3/person/popular?language=en-US&page=1"
-
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMmZlZjIwMmQ0MjFhNTYxNzg2YzU3ODQ5YzRhZmJjMyIsInN1YiI6IjY1MDFiNjcxNmEyMjI3MDBjM2I2YWIxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XJjyQmVwx0bDppP1jD0WnR_WV0eH7kBhZBRVRQFEMhQ",
-    },
-  }
-  const response = await fetch(url, options)
-  const data = await response.json()
-  return {
-    props: {
-      popularActors: data,
-    },
-  }
 }
 
 export default Actors
