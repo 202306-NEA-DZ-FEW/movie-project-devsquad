33436c3d0fb030d172d91a9c95fcfea093f91c11
takidilmi <angeldilmi@gmail.com>
2023-09-16 15:43:47 +0200

    fix(search): fix search bar

---
+++ src/pages/movies/index.jsx
@@ -0,0 +1,3 @@
+////if data is category, fetch movies by their category and show list only of the category
+//const fetch bla bla
+//if data is genres, fetch movies by genres and render only movies according to their genre
